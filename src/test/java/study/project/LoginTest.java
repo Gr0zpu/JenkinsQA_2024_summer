a15a39816bc9478025f126186855dba41baeb1d1
Maksim G <gulin.maksim@icloud.com>
2024-06-04 12:46:09 -0700

    update ci

--- src/test/java/study/project/LoginTest.java
+++ src/test/java/study/project/LoginTest.java
@@ -1,4 +1,11 @@
 package study.project;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+    @Test
+    public void CiTest() {
+        Assert.assertEquals(1,1);
+    }
 }
