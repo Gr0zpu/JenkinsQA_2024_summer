05d2cc7907b390173e358d30284268a45f24dafd
Maksim G <gulin.maksim@icloud.com>
2024-06-04 19:09:35 -0700

    update ci

--- src/test/java/study/project/LoginTest.java
+++ src/test/java/study/project/LoginTest.java
@@ -1,11 +1,21 @@
 package study.project;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import study.project.runner.BaseTest;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
     @Test
     public void CiTest() {
-        Assert.assertEquals(1,1);
+        WebDriver driver = getDriver();
+        WebElement H1 = driver.findElement(By.xpath("//h1"));
+        System.out.println(H1.getText());
+
+
+        Assert.assertEquals(H1.getText(),"Welcome to Jenkins!");
     }
 }
---
+++ src/test/java/study/project/runner/TestUtils.java
@@ -0,0 +1,4 @@
+package study.project.runner;
+
+public class TestUtils {
+}
