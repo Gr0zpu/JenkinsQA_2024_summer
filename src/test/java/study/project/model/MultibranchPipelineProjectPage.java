f1b29a539afa600608a3d1d3a7591ba9ed13f8f8
Maksim G <gulin.maksim@icloud.com>
2024-06-27 19:32:23 -0700

    MG_TC_00.005.1

---
+++ src/test/java/study/project/model/MultibranchPipelineProjectPage.java
@@ -0,0 +1,10 @@
+package study.project.model;
+
+import org.openqa.selenium.WebDriver;
+import study.project.model.base.BaseProjectPage;
+
+public class MultibranchPipelineProjectPage extends BaseProjectPage<MultibranchPipelineProjectPage> {
+    public MultibranchPipelineProjectPage(WebDriver driver) {
+        super(driver);
+    }
+}
